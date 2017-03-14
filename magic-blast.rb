c693838827b7e0e731e7b7981213f37b4628a822
fsislaben <labeed.ben-ghaly@fsis.usda.gov>
2017-03-14 10:00:05 -0500

    Update magic-blast.rb
    https now

--- magic-blast.rb
+++ magic-blast.rb
@@ -1,14 +1,14 @@
 class MagicBlast < Formula
   desc "Magic BLAST binaries"
-  homepage "http://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Web&PAGE_TYPE=BlastNews#1"
+  homepage "https://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=Web&PAGE_TYPE=BlastNews#1"
   # tag "bioinformatics"
 
   version "1.1.0"
   if OS.mac?
-    url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/magicblast/#{version}/ncbi-magicblast-#{version}-x64-macosx.tar.gz"
+    url "https://ftp.ncbi.nlm.nih.gov/blast/executables/magicblast/#{version}/ncbi-magicblast-#{version}-x64-macosx.tar.gz"
     sha256 "f962174327841a5f0528a84270b3efb96d7fcdaecba09f3349056836f31ee636"
   elsif OS.linux?
-    url "ftp://ftp.ncbi.nlm.nih.gov/blast/executables/magicblast/#{version}/ncbi-magicblast-#{version}-x64-linux.tar.gz"
+    url "https://ftp.ncbi.nlm.nih.gov/blast/executables/magicblast/#{version}/ncbi-magicblast-#{version}-x64-linux.tar.gz"
     sha256 "1e28dc19eaf03b22a94ea439c68945b48afde610c7efbe354c18139c8ad07527"
   else
     onoe "Unsupported operating system"
